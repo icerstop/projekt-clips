592cbfba5a91c80895c17bfb0f0dfcba98f3796a
gawlowskiandrzej <andrzejoklol@gmail.com>
2024-12-18 17:16:19 +0100

    add lang to bat

--- execute.bat
+++ execute.bat
@@ -2,7 +2,7 @@
 
 REM Uruchomienie aplikacji Java z odpowiednim ustawieniem ścieżki do bibliotek
 REM Można odaplić program w odpowiednim języku rosyjskim, hiszpańskim oraz japońskim
-java -Djava.library.path="./Release_lib" -Duser.language=ru -jar Release.jar
+java -Djava.library.path="./Release_lib" -jar Release.jar
 
 REM Sprawdzanie kodu zakończenia
 if %ERRORLEVEL% neq 0 (
